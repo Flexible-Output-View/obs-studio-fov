6266dc40e8f83bc097a9c28bf303b0490b34bcfd
RaphxelS <scandella@gmx.fr>
2026-09-17 10:58:52 +0200

    UPDATE: enhance macOS build scripts with CXX_FLAGS and app launch command

--- build_portable_macOS.sh
+++ build_portable_macOS.sh
@@ -8,7 +8,7 @@ rm -rf build
 fi
 
 
-cmake --preset macos -DCMAKE_BUILD_TYPE=Debug -DENABLE_WEBRTC=On -DENABLE_RELOCATABLE=ON -DENABLE_PORTABLE_CONFIG=ON
+cmake --preset macos -DCMAKE_CXX_FLAGS="-Wno-error=implicit-function-declaration" -DCMAKE_BUILD_TYPE=Debug -DENABLE_WEBRTC=On -DENABLE_RELOCATABLE=ON -DENABLE_PORTABLE_CONFIG=ON
 
 cd build_macos
 
@@ -17,7 +17,7 @@ cmake --build . --config Debug
 read -p "Run OBS now ? [y/N]: " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-./rundir/Debug/bin/obs --portable
+open ./frontend/Debug/obs.app
 else
 exit 0
 fi
--- build_portable_macOS_release.sh
+++ build_portable_macOS_release.sh
@@ -8,7 +8,7 @@ rm -rf build
 fi
 
 
-cmake --preset macos -DCMAKE_BUILD_TYPE=Release -DENABLE_WEBRTC=On -DENABLE_RELOCATABLE=ON -DENABLE_PORTABLE_CONFIG=ON .
+cmake --preset macos -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-error=implicit-function-declaration" -DENABLE_WEBRTC=On -DENABLE_RELOCATABLE=ON -DENABLE_PORTABLE_CONFIG=ON .
 
 cd build_macos
 
@@ -17,7 +17,7 @@ cmake --build . --config Release
 read -p "Run OBS now ? [y/N]: " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-./rundir/Release/bin/obs --portable
+open ./frontend/Release/obs.app
 else
 exit 0
 fi
